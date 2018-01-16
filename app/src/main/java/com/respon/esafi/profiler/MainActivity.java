0c108e39d38217e66bcac3ee3b699ddcfcc94f0f
esafif <esafif637@gmail.com>
2018-01-16 14:32:43 +0700

    Initial commit

---
+++ app/src/main/java/com/respon/esafi/profiler/MainActivity.java
@@ -0,0 +1,13 @@
+package com.respon.esafi.profiler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
